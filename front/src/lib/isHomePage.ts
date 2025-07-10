9e0ac81734eb0648f74373cb73c36e82302dee51
creativeeyestudio <contact@creative-eye.fr>
2025-07-10 16:52:02 +0000

    Fix isHomePage

--- front/src/lib/isHomePage.ts
+++ front/src/lib/isHomePage.ts
@@ -4,5 +4,5 @@ import { fetchSettings } from "./cms";
 export async function isHomePage(page: Page): Promise<boolean> {
     const settings = await fetchSettings();
     if (!settings) return false;
-    return page.id === settings.identityGroup.homepage.id;
+    return page.id === settings.identityGroup?.homepage?.id;
 }
