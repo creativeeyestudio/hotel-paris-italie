66b3a38ffdb74e9f59b48f28be9907ce6e6e993e
creativeeyestudio <contact@creative-eye.fr>
2025-07-13 10:34:11 +0000

    Languages list

--- front/src/components/layout/Header.tsx
+++ front/src/components/layout/Header.tsx
@@ -9,10 +9,20 @@ interface HeaderProps {
 const Header: React.FC<HeaderProps> = ({ locale }) => {
   return (
     <>
-      <header className="h-[64px] px-[1.25rem]">
-        <figure className="aspect-video h-full relative">
-          <Logo />
-        </figure>
+      <header className="h-[4rem] px-[1.25rem] flex justify-between items-center">
+        <div className="logo-nav h-full">
+          {/* Logo */}
+          <figure className="aspect-video h-full relative">
+            <Logo />
+          </figure>
+        </div>
+        
+        <ul className="flex gap-[0.5rem]">
+          <li><a href="">FR</a></li>
+          <li><a href="">EN</a></li>
+          <li><a href="">ES</a></li>
+          <li><a href="">IT</a></li>
+        </ul>
       </header>
       <div className="nav--primary">
         <Navigation menuId={'main-menu'} locale={locale} />
