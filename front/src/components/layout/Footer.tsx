f000b131e6a5aafee6eff83aa9d7380faba89494
creativeeyestudio <contact@creative-eye.fr>
2025-07-13 17:02:34 +0000

    Footer Mobile

--- front/src/components/layout/Footer.tsx
+++ front/src/components/layout/Footer.tsx
@@ -1,16 +1,18 @@
 import { Facebook, Instagram } from "lucide-react";
 import Link from "next/link";
 import React from "react";
+import Navigation from "./Navigation";
 
 interface FooterProps {
   locale: string;
 }
 
-const Footer: React.FC<FooterProps> = () => {
-  return <footer className="footer bg-primary text-white p-[1.25rem]">
-    <div className="footer__row--1 grid grid-cols-1 gap-[1.25rem]">
+const Footer: React.FC<FooterProps> = async ({ locale }) => {
+
+  return <footer className="footer bg-primary text-white p-[2rem]">
+    <div className="footer__row--1 grid grid-cols-1 gap-[2rem]">
       <div className="footer__block">
-        <p className="footer__title text-lg mb-[0.625rem]">Accès et contact</p>
+        <p className="footer__title text-lg mb-[1rem]">Accès et contact</p>
         <div className="grid">
           <p>Adresse</p>
           <p>
@@ -20,10 +22,11 @@ const Footer: React.FC<FooterProps> = () => {
         </div>
       </div>
       <div className="footer__block">
-        <p className="footer__title text-lg mb-[0.625rem]">Navigation</p>
+        <p className="footer__title text-lg mb-[1rem]">Navigation</p>
+        <Navigation menuId={"footer-menu"} locale={locale} />
       </div>
       <div className="footer__block">
-        <p className="footer__title text-lg mb-[0.625rem]">Suivez nous</p>
+        <p className="footer__title text-lg mb-[1rem]">Suivez nous</p>
         <ul className="flex gap-[1rem]">
           <li>
             <Facebook size={32} color="#ffffff" strokeWidth={1.5} absoluteStrokeWidth />
@@ -34,7 +37,7 @@ const Footer: React.FC<FooterProps> = () => {
         </ul>
       </div>
     </div>
-    <hr className="my-[1.25rem]" />
+    <hr className="my-[2rem]" />
     <div className="footer_row--2 text-xs leading-none">
       <p>
         <span className="flex gap-[.75rem]">
