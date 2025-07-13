2eb6a7f7ba7703f51b2f39e278b93e06b36505d1
creativeeyestudio <contact@creative-eye.fr>
2025-07-13 15:07:55 +0000

    Footer Base

--- front/src/components/layout/Footer.tsx
+++ front/src/components/layout/Footer.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 
 interface FooterProps {
@@ -5,7 +6,39 @@ interface FooterProps {
 }
 
 const Footer: React.FC<FooterProps> = () => {
-  return <footer></footer>;
+  return <footer className="footer bg-primary text-white p-[1.25rem]">
+    <div className="footer__row--1 grid grid-cols-1 gap-[1.25rem]">
+      <div className="footer__block">
+        <p className="footer__title text-lg mb-[0.625rem]">Accès et contact</p>
+        <div className="grid">
+          <p>Adresse</p>
+          <p>
+            <a href="tel:+">00 00 00 00 00</a><br/>
+            <a href="mailto:">example@xyz.fr</a>
+          </p>
+        </div>
+      </div>
+      <div className="footer__block">
+        <p className="footer__title text-lg mb-[0.625rem]">Navigation</p>
+        <div className="grid"></div>
+      </div>
+      <div className="footer__block">
+        <p className="footer__title text-lg mb-[0.625rem]">Suivez nous</p>
+      </div>
+    </div>
+    <hr className="my-[1.25rem]" />
+    <div className="footer_row--2 text-xs leading-none">
+      <p>
+        <span className="flex gap-[1rem]">
+          <Link href={""}>Mentions légales</Link>
+          <Link href={""}>Confidentialité</Link>
+          <Link href={""}>CGV</Link>
+          <span>Cookies</span>
+        </span><br />
+        Conception et développement : Com&apos; A Votre Image
+      </p>
+    </div>
+  </footer>;
 };
 
 export default Footer;
