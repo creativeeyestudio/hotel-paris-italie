c1d18b33da5cd2f95b2ef1015ad7ad8b7027fdf5
creativeeyestudio <contact@creative-eye.fr>
2025-07-16 20:52:21 +0000

    text Image Block

--- front/src/components/panels/TextImage.tsx
+++ front/src/components/panels/TextImage.tsx
@@ -5,6 +5,8 @@ import Image from "next/image";
 import React, { useEffect, useState } from "react";
 import { AspectRatio } from "../ui/aspect-ratio";
 
+import '@/styles/components/text-image.scss';
+
 const TextImage: React.FC<TextImageProps> = ({
   title,
   html,
@@ -14,13 +16,11 @@ const TextImage: React.FC<TextImageProps> = ({
   const TitleTag = firstBlock ? "h1" : "h2";
 
   const [isMobile, setIsMobile] = useState(false);
-  const [isTablet, setIsTablet] = useState(false);
   const [isDesktop, setIsDesktop] = useState(false);
 
   useEffect(() => {
     const handleResize = () => {
       setIsMobile(window.innerWidth < 768);
-      setIsTablet(window.innerWidth >= 768 && window.innerWidth < 1280);
       setIsDesktop(window.innerWidth >= 1280);
     };
 
@@ -31,11 +31,8 @@ const TextImage: React.FC<TextImageProps> = ({
   }, []);
   
   return process.env.NEXT_PUBLIC_API_URL ? (
-    <section className="
-      lg:flex lg:flex-row lg:items-stretch lg:px-[5.787vw] lg:pt-[8.681vw] last:lg:pb-[8.681vw]
-      lg:gap-x-[8.33%] even:lg:flex-row-reverse
-    ">
-    <div className="text-img__content p-[2rem] lg:p-0 lg:py-[5.787vw] w-full lg:w-5/12">
+    <section className="text-img">
+    <div className="text-img__content">
       <TitleTag className="text-img__title text-2xl md:text-3xl lg:text-4xl">
         {title}
       </TitleTag>
@@ -45,7 +42,7 @@ const TextImage: React.FC<TextImageProps> = ({
       />
     </div>
 
-    <figure className="text-img__image relative w-full lg:w-6/12">
+    <figure className="text-img__image">
       {isDesktop ? (
         <Image
           src={process.env.NEXT_PUBLIC_API_URL + image.url}
