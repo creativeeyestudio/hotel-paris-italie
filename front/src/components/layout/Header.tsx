33396937ac0069bc0f8efc98cd9db109c3563449
creativeeyestudio <contact@creative-eye.fr>
2025-07-21 18:50:18 +0000

    Nav Integrate

--- front/src/components/layout/Header.tsx
+++ front/src/components/layout/Header.tsx
@@ -2,42 +2,46 @@ import React from "react";
 import Logo from "./Logo";
 import MenuIcon from "../icons/MenuIcon";
 
+import "@/styles/layouts/header.scss";
+import Navigation from "./Navigation";
+
 interface HeaderProps {
   locale: string;
 }
 
-const Header: React.FC<HeaderProps> = () => {
+const Header: React.FC<HeaderProps> = ({ locale }) => {
   return (
     <>
-      <header className="h-[4rem] px-[1.25rem] flex justify-between items-center md:h-[6rem] md:px-[1.875rem] lg:fixed lg:z-[9999] lg:w-full lg:px-[5.787vw] lg:bg-primary lg:text-white">
-        <div className="logo-nav h-full flex items-center gap-[1.875rem]">
+      <header className="header">
+        <div className="header__nav">
           <MenuIcon />
           {/* Logo */}
-          <figure className="aspect-video h-full relative bg-white lg:px-[1rem] lg:absolute lg:left-[50%] lg:translate-x-[-50%]">
-            <div className="relative h-full">
+          <figure className="header__logo">
+            <div className="header__logo-container">
               <Logo />
             </div>
           </figure>
         </div>
 
-        <ul className="flex gap-[.5rem] divide-x divide-black lg:divide-white">
-          <li className="ps-[0.5rem]">
+        <ul className="header__lang-list">
+          <li className="header__lang-btn">
             <a href="">FR</a>
           </li>
-          <li className="ps-[0.5rem]">
+          <li className="header__lang-btn">
             <a href="">EN</a>
           </li>
-          <li className="ps-[0.5rem]">
+          <li className="header__lang-btn">
             <a href="">ES</a>
           </li>
-          <li className="ps-[0.5rem]">
+          <li className="header__lang-btn">
             <a href="">IT</a>
           </li>
         </ul>
       </header>
-      {/* <div className="nav--primary">
+
+      <div className="nav--primary">
         <Navigation menuId={'main-menu'} locale={locale} />
-      </div> */}
+      </div>
     </>
   );
 };
