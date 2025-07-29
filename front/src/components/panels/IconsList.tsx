9e9350e8cec8e91f6749e0015bc9dda07924c43e
creativeeyestudio <contact@creative-eye.fr>
2025-07-29 19:41:43 +0000

    Prettier fix

--- front/src/components/panels/IconsList.tsx
+++ front/src/components/panels/IconsList.tsx
@@ -1,11 +1,19 @@
-'use client'
+"use client";
 
 import React from "react";
 import Icon from "../icons/Icon";
 import { motion } from "framer-motion";
 
 interface Service {
-  serviceIcon: 'bathroom' | 'air' | 'wifi' | 'phone' | 'safe' | 'minibar' | 'television' | 'courtesyTray';
+  serviceIcon:
+    | "bathroom"
+    | "air"
+    | "wifi"
+    | "phone"
+    | "safe"
+    | "minibar"
+    | "television"
+    | "courtesyTray";
   serviceLabel: string;
 }
 
@@ -14,26 +22,25 @@ interface IconsListProps {
 }
 
 const IconsList: React.FC<IconsListProps> = ({ serviceList }) => {
-  
-    if (!Array.isArray(serviceList) || serviceList.length === 0) return null;
-    
-    return (
-        <section className="icons-list">
-        {serviceList.map((service, index) => (
-            <motion.div
-              initial={{ y: 200, opacity: 0 }}
-              whileInView={{ y: 0, opacity: 1 }}
-              transition={{ duration: 0.5, ease: "easeOut" }}
-              viewport={{ once: false, amount: 0.1 }}
-              className="icons-list__item" 
-              key={index}
-            >
-              <Icon type={service.serviceIcon} className="icons-list__icon" />
-              <span className="icons-list__label">{service.serviceLabel}</span>
-            </motion.div>
-        ))}
-        </section>
-    );
+  if (!Array.isArray(serviceList) || serviceList.length === 0) return null;
+
+  return (
+    <section className="icons-list">
+      {serviceList.map((service, index) => (
+        <motion.div
+          initial={{ y: 200, opacity: 0 }}
+          whileInView={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.5, ease: "easeOut" }}
+          viewport={{ once: false, amount: 0.1 }}
+          className="icons-list__item"
+          key={index}
+        >
+          <Icon type={service.serviceIcon} className="icons-list__icon" />
+          <span className="icons-list__label">{service.serviceLabel}</span>
+        </motion.div>
+      ))}
+    </section>
+  );
 };
 
 export default IconsList;
